34b2423c95a54c517b9d33a63d8a4f227665a8ce
Heather Korczynski <hk@Heathers-MacBook-Pro.local>
2022-10-01 19:50:56 -0500

    Added window prompts and list arrays

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,39 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("button click");
+  var passwordLength = window.prompt("How many characters in your password?");
+
+  if (passwordLength >= 1 && passwordLength <= 128) {
+    window.alert("dope");
+  } else {
+    window.alert("Please enter a number between 1 and 128.");
+  }
+
+  var passwordLowercase = window.confirm("Would you like to include lowercase?");
+  
+  
+  var passwordUppercase = window.confirm("Would you like to include uppercase?");
+  
+  
+  var passwordNumbers = window.confirm("Would you like to include numbers?");
+  
+  
+  var passwordSpecialChar = window.confirm("Would you like to include special characters?");
+
+
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  
+  
+  
+  return "passwordText";
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,3 +48,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// if ()
